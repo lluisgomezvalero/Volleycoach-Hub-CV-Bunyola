57c457e09a3f93624dd09d3076f034e4187b22e6
lluisgomezvalero <lluisgomez5@gmail.com>
2026-08-27 13:18:36 +0200

    Aplicar corrección responsive de bienestar

--- react-migration/src/main.jsx
+++ react-migration/src/main.jsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './auth/AuthProvider.jsx';
 import './styles.css';
 import './theme.css';
 import './pages/TeamAttendanceProfessional.css';
+import './pages/WellnessMobileFix.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
