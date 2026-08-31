80b410bb3034144d3fcb64cef48ac778d8208bea
lluisgomezvalero <lluisgomez5@gmail.com>
2026-08-31 16:12:26 +0200

    Load wellness polish styles

--- react-migration/src/main.jsx
+++ react-migration/src/main.jsx
@@ -14,6 +14,7 @@ import './pages/TrainingPolish.css';
 import './pages/CalendarPolish.css';
 import './pages/TeamAttendanceProfessional.css';
 import './pages/WellnessMobileFix.css';
+import './pages/WellnessPolish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
