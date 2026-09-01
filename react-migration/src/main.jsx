b190048728310b93811d1b669b1e788cf9b2ddd0
lluisgomezvalero <lluisgomez5@gmail.com>
2026-09-01 14:24:05 +0200

    Mount required wellness selection guard

--- react-migration/src/main.jsx
+++ react-migration/src/main.jsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './auth/AuthProvider.jsx';
 import WeeklyTeamWellness from './components/WeeklyTeamWellness.jsx';
 import TodayWellnessAlerts from './components/TodayWellnessAlerts.jsx';
 import WellnessPlayerCheckin from './components/WellnessPlayerCheckin.jsx';
+import WellnessRequiredSelectionGuard from './components/WellnessRequiredSelectionGuard.jsx';
 import CalendarSwipeEnhancer from './components/CalendarSwipeEnhancer.jsx';
 import TrainingPlayerRpeBreakdown from './components/TrainingPlayerRpeBreakdown.jsx';
 import CrossAccountIdentitySync from './components/CrossAccountIdentitySync.jsx';
@@ -38,6 +39,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <WeeklyTeamWellness />
         <TodayWellnessAlerts />
         <WellnessPlayerCheckin />
+        <WellnessRequiredSelectionGuard />
         <CalendarSwipeEnhancer />
         <TrainingPlayerRpeBreakdown />
         <CrossAccountIdentitySync />
