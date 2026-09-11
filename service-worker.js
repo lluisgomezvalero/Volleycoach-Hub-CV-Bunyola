0a89538cde2297490caab5abb21871b5973a37ac
lluisgomezvalero <lluisgomez5@gmail.com>
2026-09-11 13:09:33 +0200

    Renueva la caché de la PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'volleycoach-react-pwa-';
-const CACHE_VERSION = '20260903-auth-push-safety-v1';
+const CACHE_VERSION = '20260911-cache-recovery-v1';
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const OFFLINE_SHELL = ['./', './manifest.webmanifest'];
 
